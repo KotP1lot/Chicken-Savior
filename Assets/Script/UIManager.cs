922464a425c048674fc8987c87f4771751601e1f
Bohdan Dzyubanchuk <tahkictik123@gmail.com>
2023-07-09 15:46:12 +0300

    Pun

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -4,19 +4,39 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    string[] finaltext = new string[] {
+        "Don't chick-en up, try again!",
+        "Feather-brained move.",
+    "That wasn't egg-cellent.",
+    "Give it another shot, you're eggs-tremely close!",
+    "You chicken out! Don't worry, you'll get another clucking chance.",
+    "Winging it didn't work this time.",
+    "Oops.. Chiken for dinner today."};
     [SerializeField]TextMeshProUGUI scoreText;
     [SerializeField]TextMeshProUGUI topScoreText;
+    [SerializeField]TextMeshProUGUI finalText;
     [SerializeField]GameObject endPanel;
+    [SerializeField]GameObject pausePanel;
     Animator animator;
     EventSyst syst;
+    bool isEnded = false;
     int score = 0;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isEnded) 
+        {
+            OnPause();
+        }
+    }
     void OnDead(TypeDie typeDie)
     {
+        finalText.text = finaltext[Random.Range(0,finaltext.Length)];
+        isEnded = true;
         int preScore = PlayerPrefs.GetInt("BestScore", 0);
         if (score >= preScore) 
         {
@@ -30,7 +50,6 @@ public class UIManager : MonoBehaviour
     public void OnRestart() 
     {
         SceneManager.LoadScene(1);
-    
     }
 
     void OnStepForward() 
@@ -48,5 +67,20 @@ public class UIManager : MonoBehaviour
     {
         syst.OnDead -= OnDead;
         syst.OnStepForward -= OnStepForward;
+    }
+    public void OnPause() 
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+    public void OnResume() 
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+    public void BackMainMenu() 
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
 }
